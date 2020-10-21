00383c0b71ef443fcbdc77e47ecd32368fc34e4b
zerochenkun <chenuvi@outlook.com>
2020-10-21 10:43:48 +0800

    refactor: Money.vue 使用Vuex 记录 record

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    <!-- {{recordList}}-->
+     {{$store.state.recordList}}
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <div class="notes">
@@ -20,15 +20,18 @@
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
-  import store from '@/store/index2';
-
 
   @Component({
-    components: {Tags, FormItem, Types, NumberPad}
+    components: {Tags, FormItem, Types, NumberPad},
+    computed:{
+      recordList(){
+        return this.$store.state.recordList
+      }
+    }
   })
   export default class Money extends Vue {
-    tags = store.tagList;
-    recordList = store.recordList;
+    // tags = $store.state.tagList;
+
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
@@ -38,7 +41,10 @@
     }
 
     saveRecord() {
-      store.createRecord(this.record)
+      this.$store.commit('createRecord',this.record)
+    }
+    created(){
+      this.$store.commit('fetchRecords')
     }
   }
 </script>
