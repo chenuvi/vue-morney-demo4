7593a96637d60fffe782932a129429fc20c68153
zerochenkun <chenuvi@outlook.com>
2020-10-21 16:04:43 +0800

    refactor: function createTag 抽离为 mixin

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -4,7 +4,7 @@
       <router-link class="tag"
                    v-for="tag in tagList" :key="tag.id"
                    :to="`/labels/edit/${tag.id}`">
-        <span>{{tag.name}}</span>
+        <span>{{ tag.name }}</span>
         <Icon name="right"/>
       </router-link>
     </div>
@@ -18,64 +18,62 @@
 </template>
 
 <script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import {mixins} from 'vue-class-component';
+import Button from '@/components/Button.vue';
+import {TagHelper} from '@/Mixins/TagHelper';
 
-  import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
-  import Button from '@/components/Button.vue';
-
-  @Component({
-    components: {Button},
-    computed:{
-      tagList(){
-        return this.$store.state.tagList
-      }
-    }
-  })
-  export default class Labels extends Vue {
-    created() {
-      this.$store.commit('fetchTag');
-    }
-    createTag() {
-      const name = window.prompt('请输入标签名');
-      if (!name) {
-        return window.alert('标签名不能为空');
-      }
-      this.$store.commit('createTag', name);
+@Component({
+  components: {Button},
+  computed: {
+    tagList() {
+      return this.$store.state.tagList;
     }
   }
+})
+export default class Labels extends mixins(TagHelper) {
+  created() {
+    this.$store.commit('fetchTag');
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-  .tags {
-    background: white;
-    font-size: 16px;
-    padding-left: 16px;
-    > .tag {
-      min-height: 44px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      border-bottom: 1px solid #e6e6e6;
-      svg {
-        width: 18px;
-        height: 18px;
-        color: #666;
-        margin-right: 16px;
-      }
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+
+  > .tag {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+
+    svg {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
     }
   }
-  .createTag {
-    background: #767676;
-    color: white;
-    border-radius: 4px;
-    border: none;
-    height: 40px;
-    padding: 0 16px;
-    &-wrapper {
-      text-align: center;
-      padding: 16px;
-      margin-top: 44-16px;
-    }
+}
+
+.createTag {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
   }
+}
 </style>
 
