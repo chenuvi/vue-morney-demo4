6cc35800dca976ab0041a9d7275a29225969e575
zerochenkun <chenuvi@outlook.com>
2020-10-21 11:16:13 +0800

    refactor: Tags.vue 使用Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -6,7 +6,7 @@
     <ul class="current">
       <li v-for="tag in tagList" :key="tag.id"
           :class="{selected: selectedTags.indexOf(tag)>=0}"
-          @click="toggle(tag)">{{tag.name}}
+          @click="toggle(tag)">{{ tag.name }}
       </li>
     </ul>
   </div>
@@ -14,70 +14,84 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {Component, Prop} from 'vue-property-decorator';
-  import store from '@/store/index2';
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
 
-  @Component
-  export default class Tags extends Vue {
-    tagList = store.fetchTag()
-    selectedTags: string[] = [];
-
-    toggle(tag: string) {
-      const index = this.selectedTags.indexOf(tag);
-      if (index >= 0) {
-        this.selectedTags.splice(index, 1);
-      } else {
-        this.selectedTags.push(tag);
-      }
-      // this.$emit('update:value', this.selectedTags);
-    }
-
-    create() {
-      const name = window.prompt('请输入标签名');
-      if (!name) {return window.alert('标签名不能为空'); }
-      store.createTag(name)
+@Component({
+  computed: {
+    tagList() {
+      return this.$store.state.tagList;
     }
   }
+})
+export default class Tags extends Vue {
+  selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit('fetchTag');
+  }
+
+  toggle(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
+    }
+  }
+
+  create() {
+    const name = window.prompt('请输入标签名');
+    if (!name) {
+      return window.alert('标签名不能为空');
+    }
+    this.$store.commit('createTag', name);
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-  .tags {
-    background: white;
-    font-size: 14px;
-    padding: 16px;
-    flex-grow: 1;
+.tags {
+  background: white;
+  font-size: 14px;
+  padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+
+  > .current {
     display: flex;
-    flex-direction: column-reverse;
-    > .current {
-      display: flex;
-      flex-wrap: wrap;
-      > li {
-        $bg: #d9d9d9;
-        background: $bg;
-        $h: 24px;
-        height: $h;
-        line-height: $h;
-        border-radius: $h/2;
-        padding: 0 16px;
-        margin-right: 12px;
-        margin-top: 4px;
-        &.selected {
-          background: darken($bg, 50%);
-          color: white;
-        }
-      }
-    }
-    > .new {
-      padding-top: 16px;
-      button {
-        background: transparent;
-        border: none;
-        color: #999;
-        border-bottom: 1px solid;
-        padding: 0 4px;
+    flex-wrap: wrap;
+
+    > li {
+      $bg: #d9d9d9;
+      background: $bg;
+      $h: 24px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 16px;
+      margin-right: 12px;
+      margin-top: 4px;
+
+      &.selected {
+        background: darken($bg, 50%);
+        color: white;
       }
     }
   }
+
+  > .new {
+    padding-top: 16px;
+
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
 
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import clone from '@/lib/clone';
+import createId from '@/lib/createId';
 
 Vue.use(Vuex)  // 把 store 绑到 Vue.prototype.$store = $store 上
 
 const store =  new Vuex.Store({
   state: {
     recordList: [] as RecordItem[],
+    tagList: [] as Tag[],
   },
   mutations: {
+    // record
     fetchRecords(state) {
       state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
       return state.recordList;
@@ -21,6 +24,59 @@ const store =  new Vuex.Store({
     },
     saveRecords() {
       window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+    },
+
+    // tag
+    fetchTag(state) {
+      state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+      return state.tagList;
+    },
+    // findTag(id: string) {
+    //   return this.tagList.filter(t => t.id === id)[0];
+    // },
+    createTag(state,name: string) {
+      // this.data = [{id:'1', name:'1'}, {id:'2', name:'2'}]
+      const names = state.tagList.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        window.alert('标签名重复了');
+        return 'duplicated';
+      } else {
+        const id = createId().toString();
+        state.tagList.push({id, name: name});
+        store.commit('saveTag')
+        return 'success';
+      }
+    },
+    // updateTag(id: string, name: string) {
+    //   const idList = this.tagList.map(item => item.id);
+    //   if (idList.indexOf(id) >= 0) {
+    //     const names = this.tagList.map(item => item.name);
+    //     if (names.indexOf(name) >= 0) {
+    //       return 'duplicated';
+    //     } else {
+    //       const tag = this.tagList.filter(item => item.id === id)[0];
+    //       tag.name = name;
+    //       this.saveTag();
+    //       return 'success';
+    //     }
+    //   } else {
+    //     return 'not found';
+    //   }
+    // },
+    // removeTag(id: string) {
+    //   let index = -1;
+    //   for (let i = 0; i < this.tagList.length; i++) {
+    //     if (this.tagList[i].id === id) {
+    //       index = i;
+    //       break;
+    //     }
+    //   }
+    //   this.tagList.splice(index, 1);
+    //   this.saveTag();
+    //   return true;
+    // },
+    saveTag() {
+      window.localStorage.setItem('tagList', JSON.stringify(this.tagList));
     }
   }
 })
