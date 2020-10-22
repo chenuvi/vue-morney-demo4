d66360402cd2c2348cafab7ff9079fb5c03773dd
zerochenkun <chenuvi@outlook.com>
2020-10-22 20:53:23 +0800

    feat:抽离 Tabs.vue，使用在 Statistics.vue 里面

--- src/store/index2.ts
+++
@@ -1,9 +0,0 @@
-import tagStore from '@/store/tagStore';
-import recordStore from '@/store/recordStore';
-
-let store = {
-    ...tagStore,
-    ...recordStore
-};
-
-export default store
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,30 @@
 <template>
   <Layout>
-    Statistics.vue
+    <Tabs :dataSource="typeList" :value.sync="type" class-prefix="type"></Tabs>
+    <Tabs :dataSource="intervalList" :value.sync="interval" class-prefix="interval"></Tabs>
   </Layout>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Statistics',
-  };
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Tabs from '@/views/Tabs.vue';
+
+@Component({
+  components: {Tabs}
+})
+export default class Statistics extends Vue {
+  type = '-';
+  interval = 'day';
+  intervalList = [
+    {text: '按天', value: 'day'},
+    {text: '按周', value: 'week'},
+    {text: '按月', value: 'month'},
+  ];
+  typeList = [
+    {text: '支出', value: '-'},
+    {text: '收入', value: '+'},
+  ];
+}
 </script>
 
